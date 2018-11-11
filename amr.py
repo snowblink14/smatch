d5c62ada3ff09e526ae73ad54afe798de493699a
Deng Cai <thisisjcykcd@gmail.com>
2018-11-11 19:13:58 +0800

    update description

--- amr.py
+++ amr.py
@@ -207,12 +207,12 @@ class AMR(object):
         exceptions =set(["prep-on-behalf-of", "prep-out-of", "consist-of", "subset-of"])
         def update_triple(node_relation_dict, (u, r, v)):
             # we detect a relation (r) between u and v, with direction u to v.
-            # if relation name ends with "-of", e.g."arg0-of",
+            # in most cases, if relation name ends with "-of", e.g."arg0-of",
             # it is reverse of some relation. For example, if a is "arg0-of" b,
             # we can also say b is "arg0" a.
             # If the relation name ends with "-of", we store the reverse relation.
-            # also note some relations like "prep-on-behalf-of" and "prep-out-of"
-            # also note realtion "mode" is the reverse of "domain"
+            # but note some relations like "prep-on-behalf-of" and "prep-out-of"
+            # also note realtion "mod" is the reverse of "domain"
             if r.endswith("-of") and not r in exceptions:
                 node_relation_dict[v].append((r[:-3], u))
             elif r=="mod":
