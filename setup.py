a4f2e28a16666860bf70c4274803a60c67bac2cb
Michael Wayne Goodman <goodman.m.w@gmail.com>
2020-01-13 14:59:07 +0800

    Update README and CHANGELOG (#24)
    * Update README and CHANGELOG
    
    This expands the README.md with more information and links, and
    renames and reformats update_log to CHANGELOG.md. It also adds PyPI
    project classifiers and metadata so the README is properly rendered at
    https://pypi.org/project/smatch/.
    
    * Resolve comments from PR review
    
    - Adjust wording in README.md intro
    - Fix link text in README.md's related projects
    - Add "evaluation" as a keyword in `setup.py`
    
    * Swap order of test/gold in README
    
    * Remove unused imports from setup.py
    
    * Update CHANGELOG.md with recent commits
    
    * Update CHANGELOG; fix PyPI link

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 
-import sys
 import os
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from disutils.core import setup
+
+from setuptools import setup
 
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -18,11 +15,28 @@ setup(name="smatch",
       setup_requires=['setuptools_scm'],
       description="Smatch (semantic match) tool",
       long_description=README,
+      long_description_content_type='text/markdown',
       author="Shu Cai",
       author_email="shucai.work@gmail.com",
       url="https://github.com/snowblink14/smatch",
       license="MIT",
+      classifiers=[
+          'Development Status :: 5 - Production/Stable'
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Information Technology',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Text Processing :: Linguistic',
+          'Topic :: Utilities'
+      ],
+      keywords='nlp semantics amr evaluation',
       py_modules=["smatch", "amr"],
       scripts=["smatch.py"],
       )
-
